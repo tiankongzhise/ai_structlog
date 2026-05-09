183d9efae2376a9655caf2f7987c0fbef8aa113a
3700x <tiankongzhise@163.com>
2026-05-09 12:10:03 +0800

    修正正式打包tag

--- src/tkzs_structlog/_version.py
+++ src/tkzs_structlog/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.1.dev44+g0b7310855"
-__version_tuple__ = version_tuple = (0, 1, "dev44", "g0b7310855")
+__version__ = version = '0.1.dev55+g2525beeb3'
+__version_tuple__ = version_tuple = (0, 1, 'dev55', 'g2525beeb3')
 
 __commit_id__ = commit_id = None
